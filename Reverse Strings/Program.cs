0d3738e029ebc6f44726dd34a1417f7991500c57
Dimityr <dzambazov1986@gmail.com>
2024-03-09 08:44:38 +0200

    Add project files.

---
+++ Reverse Strings/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+while (input != "end")
+{
+    string reversed = string.Join("", input.Reverse().ToArray());
+
+    Console.WriteLine(input + " = " + reversed);
+
+    input = Console.ReadLine();
+
+}
